e5a176389051a23aac77937e796c1edf105b2822
ArikaWakasugi <ark.tooyoung@gmail.com>
2021-06-27 17:03:18 +0900

    [Add]box2

--- app.js
+++ app.js
@@ -6,3 +6,12 @@ $(function(){
     $('.box1').removeClass('box1-ext');
   });
 });
+
+$(function(){
+  $('.box2').on('click',function(){
+    $('.box2').addClass('box2-ext');
+  });
+  $('.box2').mouseout(function(){
+    $('.box2').removeClass('box2-ext');
+  });
+});
